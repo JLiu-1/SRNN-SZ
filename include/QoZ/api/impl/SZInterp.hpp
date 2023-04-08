27d69a8fc54a2df65d8ae98ddc2bfa07dbcb0368
meso272 <liujinyang1201@126.com>
2023-04-08 13:14:59 -0700

    misc tests

--- include/QoZ/api/impl/SZInterp.hpp
+++ include/QoZ/api/impl/SZInterp.hpp
@@ -1846,10 +1846,12 @@ double Tuning(QoZ::Config &conf, T *data){
 
     size_t num_filtered_blocks=starts.size();
     double profiling_coeff=1;
+    //
     if(conf.profiling){
         profiling_coeff=((double)num_filtered_blocks)/(totalblock_num);
-
+        std::cout<<profiling_coeff<<std::endl;
     }
+    //
     std::vector<size_t> global_dims=conf.dims;
     size_t global_num=conf.num;
 
@@ -1942,7 +1944,7 @@ double Tuning(QoZ::Config &conf, T *data){
                     conf.setDims(coeffs_size.begin(),coeffs_size.end());
 
                 }
-                //std::cout<<"dawd"<<std::endl;
+                std::cout<<"dawd"<<std::endl;
 
             }
           // std::cout<<"a3"<<std::endl;
