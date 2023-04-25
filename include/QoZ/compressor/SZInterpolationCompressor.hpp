e13ab3d94f75981615605cb0578e6ed7cffa52ef
meso272 <liujinyang1201@126.com>
2023-04-24 17:32:16 -0700

    remove reduandant

--- include/QoZ/compressor/SZInterpolationCompressor.hpp
+++ include/QoZ/compressor/SZInterpolationCompressor.hpp
@@ -3365,7 +3365,7 @@ namespace QoZ {
                             const std::string &interp_func, const int direction, size_t stride = 1,int tuning=0,size_t cross_block=0) {
             double predict_error = 0;
             size_t stride2x = stride * 2;
-            、、max_error = 0;
+            //max_error = 0;
             const std::array<int, N> dims = dimension_sequences[direction];
             for (size_t j = (begin[dims[1]] ? begin[dims[1]] + stride2x : 0); j <= end[dims[1]]; j += stride2x) {
                 for (size_t k = (begin[dims[2]] ? begin[dims[2]] + stride2x : 0); k <= end[dims[2]]; k += stride2x) {
