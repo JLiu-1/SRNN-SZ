9cc1503c449f4dd526f5b825123d81f206e2de8e
meso272 <liujinyang1201@126.com>
2023-04-25 20:32:46 -0700

    sperr 2d

--- include/QoZ/preprocessor/pywt_wrapper.py
+++ include/QoZ/preprocessor/pywt_wrapper.py
@@ -7,10 +7,10 @@ structure = None
 
 
 def dwt(data, wave_type,is_float):
-    start = time.time()
+    #start = time.time()
     c = pywt.wavedecn(data, wave_type, mode="periodization")
     d = pywt.coeffs_to_array(c)
-    end = time.time()
+    #end = time.time()
    # print('pure python time without c++ binding', end - start)
     global shape, structure
     structure = pickle.dumps(d[1])
@@ -29,20 +29,20 @@ def dwt_structure():
 
 
 def idwt(data, wave_structure, wave_type, ori_shape,is_float):
-    start = time.time()
+    #start = time.time()
     structure = pickle.loads(wave_structure)
-    #print(structure)
-    #print(data.shape)
+    print(structure)
+    print(data.shape)
     dc_c = pywt.array_to_coeffs(data, structure)
-    #print(ori_shape)
+    print(ori_shape)
     b = pywt.waverecn(dc_c, wave_type, mode="periodization")
-    #print(b.shape)
+    print(b.shape)
     if b.shape != ori_shape:
         if len(ori_shape) == 2:
             b = b[:ori_shape[0], :ori_shape[1]]
         else:
             b = b[:ori_shape[0], :ori_shape[1], :ori_shape[2]]
-    end = time.time()
+    #end = time.time()
     #print('pure python time without c++ binding', end - start)
     if is_float:
         return b.astype(np.float32)
