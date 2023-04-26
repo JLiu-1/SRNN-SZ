19196788a2f5f67c676d50b6538f78c7c80f9f4f
meso272 <liujinyang1201@126.com>
2023-04-25 19:45:11 -0700

    sperr 2d

--- test/qoz.cpp
+++ test/qoz.cpp
@@ -530,8 +530,9 @@ int main(int argc, char *argv[]) {
         if (dataType == SZ_FLOAT) {
             compress<float>(inPath, cmpPath, conf);
         } 
-        /*else if (dataType == SZ_DOUBLE) {
+        else if (dataType == SZ_DOUBLE) {
             compress<double>(inPath, cmpPath, conf);
+        /*
         } else if (dataType == SZ_INT32) {
             compress<int32_t>(inPath, cmpPath, conf);
         } else if (dataType == SZ_INT64) {
@@ -557,9 +558,11 @@ int main(int argc, char *argv[]) {
             decompress<float>(inPath, cmpPath, decPath, conf, binaryOutput, printCmpResults);
 
         } 
-        /*else if (dataType == SZ_DOUBLE) {
+        else if (dataType == SZ_DOUBLE) {
             decompress<double>(inPath, cmpPath, decPath, conf, binaryOutput, printCmpResults);
-        } else if (dataType == SZ_INT32) {
+
+        } 
+        /*else if (dataType == SZ_INT32) {
             decompress<int32_t>(inPath, cmpPath, decPath, conf, binaryOutput, printCmpResults);
         } else if (dataType == SZ_INT64) {
             decompress<int64_t>(inPath, cmpPath, decPath, conf, binaryOutput, printCmpResults);
