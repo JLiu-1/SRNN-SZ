210cd4f35ddff26e311125b62cecf1c431fe33fe
meso272 <liujinyang1201@126.com>
2023-05-05 22:44:52 -0700

    regressive interp

--- include/QoZ/utils/Interpolators.hpp
+++ include/QoZ/utils/Interpolators.hpp
@@ -32,8 +32,8 @@ namespace QoZ {
 
     template<class T>
     inline T interp_cubic(T a, T b, T c, T d) {
-        return (-a + 9 * b + 9 * c - d) / 16;
-        //return -0.06368435202786181*a+0.5731591682507563*b+0.5731591682507563*c-0.06368435202786181*d;
+        //return (-a + 9 * b + 9 * c - d) / 16;
+        return -0.06368435202786181*a+0.5731591682507563*b+0.5731591682507563*c-0.06368435202786181*d;
     }
 
     template<class T>
