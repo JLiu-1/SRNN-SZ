ff5988f4ade8d780840a4260ccd29e0a99c3971e
meso272 <liujinyang1201@126.com>
2023-05-07 05:27:28 -0700

    cubic test

--- include/QoZ/utils/Interpolators.hpp
+++ include/QoZ/utils/Interpolators.hpp
@@ -45,18 +45,27 @@ namespace QoZ {
     }
 
     template<class T>
-    inline T interp_cubic(T a, T b, T c, T d) {
-        //return (-a + 9 * b + 9 * c - d) / 16;
+    inline T interp_cubic_1(T a, T b, T c, T d) {
+        return (-a + 9 * b + 9 * c - d) / 16;//noknot
         //return -0.06368435202786181*a+0.5731591682507563*b+0.5731591682507563*c-0.06368435202786181*d;
-        return (-3*a+23*b+23*c-3*d)/40;
+        //return (-3*a+23*b+23*c-3*d)/40;
     }
 
     template<class T>
-    inline T interp_cubic_2(T a, T b, T c, T d,T e,T f) {
+    inline T interp_cubic_2(T a, T b, T c, T d) {
+        return (-3 * a + 23 * b + 23 * c - 3 * d) / 40;//nat
+    }
+
+    template<class T>
+    inline T interp_cubic_adj(T a, T b, T c, T d,T e,T f) {//adj6 nat
         return (3*a-18*b+46*c+46*d-18*e+3*f)/62;
     }
     template<class T>
-    inline T interp_cubic_3(T a, T b, T c, T d) {
+    inline T interp_cubic_adj_2(T a, T b, T c, T d,T e,T f) {//adj6/4 noknot
+        return (-b+4*c+4*d-e)/6;
+    }
+    template<class T>
+    inline T interp_cubic_adj_3(T a, T b, T c, T d) {//adj4 nat
         return (-3*a+11*b+11*c-3*d)/16;
     }
 
@@ -120,10 +129,7 @@ namespace QoZ {
         return (-5 * a + 21 * b - 35 * c + 35 * d) / 16;
     }
 
-    template<class T>
-    inline T interp_cubic2(T a, T b, T c, T d) {
-        return (-3 * a + 23 * b + 23 * c - 3 * d) / 40;
-    }
+    
 
     template<class T>
     inline T interp_akima(T a, T b, T c, T d) {//what is it?
