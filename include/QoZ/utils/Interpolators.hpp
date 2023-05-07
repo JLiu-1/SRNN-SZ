2708719eedb6c7831906ae67534e6f90005a2da4
meso272 <liujinyang1201@126.com>
2023-05-06 17:07:28 -0700

    cubic test

--- include/QoZ/utils/Interpolators.hpp
+++ include/QoZ/utils/Interpolators.hpp
@@ -34,7 +34,7 @@ namespace QoZ {
     inline T interp_cubic(T a, T b, T c, T d) {
         //return (-a + 9 * b + 9 * c - d) / 16;
         //return -0.06368435202786181*a+0.5731591682507563*b+0.5731591682507563*c-0.06368435202786181*d;
-        return (-1*a+17*b+17*c-1*d)/32;
+        return (-3*a+35*b+35*c-3*d)/64;
     }
 
     template<class T>
