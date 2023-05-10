090a417d4f4dbff5595581dc6d2d4ae71c98a0cb
meso272 <liujinyang1201@126.com>
2023-05-09 17:44:47 -0700

    update

--- include/QoZ/utils/Config.hpp
+++ include/QoZ/utils/Config.hpp
@@ -51,8 +51,8 @@ namespace QoZ {
         uint8_t cubicSplineType = 0;//noknot,nat
         uint8_t interpDirection = 0;//0,N!-1
         uint8_t adjInterp=0;//0,1
-        std::array<double,3> dimCoeffs={1/3,1/3,1/3};
-
+        std::array<double,3> dimCoeffs={1.0/3.0,1.0/3.0,1.0/3.0};
+ 
     };
 
     void print_meta(Interp_Meta meta){
