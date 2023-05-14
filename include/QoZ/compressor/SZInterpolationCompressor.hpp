7c7e2a2fa51e3ddc0828d0f1d0bb70016f907175
meso272 <liujinyang1201@126.com>
2023-05-14 12:24:42 -0700

    blockwise tuning

--- include/QoZ/compressor/SZInterpolationCompressor.hpp
+++ include/QoZ/compressor/SZInterpolationCompressor.hpp
@@ -665,9 +665,9 @@ namespace QoZ {
                                             cur_meta.adjInterp=adj_interp;
                                             cur_block=orig_sampled_block;
                                             
-                                            double cur_loss=block_interpolation(cur_block.data(), sb_starts, sb_ends, PB_predict_overwrite,
-                                                                               interpolators[cur_meta.interpAlgo],cur_meta, 1,2,0,0);//,cross_block,regressiveInterp);
-                                            //double cur_loss=0.0;
+                                            //double cur_loss=block_interpolation(cur_block.data(), sb_starts, sb_ends, PB_predict_overwrite,
+                                            //                              interpolators[cur_meta.interpAlgo],cur_meta, 1,2,0,0);//,cross_block,regressiveInterp);
+                                            double cur_loss=0.0;
                                             if(cur_loss<best_loss){
                                                 best_loss=cur_loss;
                                                 best_meta=cur_meta;
