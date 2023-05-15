3f6d5335a71d8ccf7ddc851ce87fef32c331a93a
meso272 <liujinyang1201@126.com>
2023-05-14 22:17:27 -0700

    coeff interp

--- include/QoZ/utils/Sample.hpp
+++ include/QoZ/utils/Sample.hpp
@@ -150,20 +150,20 @@ template<class T, uint N>
                     for (size_t k = 3; k < dimz-3; k+=stride) {
                         if(i%stride2x==0 and j%stride2x==0 and k%stride2x==0)
                             continue;
-                        std::cout<<i<<" "<<j<<" "<<k<<std::endl;
+                        //std::cout<<i<<" "<<j<<" "<<k<<std::endl;
                         //count+=1;
                         size_t idx=i*dimyz+j*dimz+k;
                         T *d= data+idx;
                         T cur_value=*d;
-                        std::cout<<cur_value<<std::endl;
+                        //std::cout<<cur_value<<std::endl;
                         std::vector<double>temp_xs={*(d - 3*dimyz),*(d - dimyz),*(d + dimyz), *(d + 3*dimyz),*(d - 3*dimz),*(d - dimz),*(d + dimz),*(d + 3*dimz),*(d - 3), *(d - 1), *(d + 1), *(d + 3)};
-                        std::cout<<"dwa"<<std::endl;
+                        //std::cout<<"dwa"<<std::endl;
                         std::vector<double>temp_ys={cur_value,cur_value,cur_value};
-                        std::cout<<"dwa2"<<std::endl;
+                       // std::cout<<"dwa2"<<std::endl;
                         xs.insert(xs.end(),temp_xs.begin(),temp_xs.end());
-                        std::cout<<"dwa3"<<std::endl;
-                        ys.insert(xs.end(),temp_ys.begin(),temp_ys.end());
-                        std::cout<<"dwa4"<<std::endl;
+                        //std::cout<<"dwa3"<<std::endl;
+                        ys.insert(ys.end(),temp_ys.begin(),temp_ys.end());
+                        //std::cout<<"dwa4"<<std::endl;
                        
 
                     }
