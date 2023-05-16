6368f24f82360e44659719dbcc03c8bdc131fd99
meso272 <liujinyang1201@126.com>
2023-05-16 11:32:03 -0700

    md_block_refine

--- include/QoZ/compressor/SZInterpolationCompressor.hpp
+++ include/QoZ/compressor/SZInterpolationCompressor.hpp
@@ -2006,7 +2006,7 @@ namespace QoZ {
             else{//cubic
 
                 if(n<5){//m>=5
-                    size_t begin=begin1+begin2+stride1,end=begin+(m-1)*stride2+stride1;
+                    size_t begin=begin1+begin2+stride1,end=begin+(m-1)*stride2;
                     for(size_t i=1;i<n;i+=2){
                         
                         predict_error+=block_interpolation_1d(data,  begin, end,  stride2,interp_func,pb,meta,tuning);
@@ -2016,7 +2016,7 @@ namespace QoZ {
                     return predict_error;
                 }
                 else if(m<5){//n>=5
-                    size_t begin=begin1+begin2+stride2,end=begin+(n-1)*stride1+stride2;
+                    size_t begin=begin1+begin2+stride2,end=begin+(n-1)*stride1;
                     for(size_t j=1;j<m;j+=2){
                         
                         predict_error+=block_interpolation_1d(data,  begin, end,  stride1,interp_func,pb,meta,tuning);
