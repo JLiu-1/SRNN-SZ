81af23333a1768f7dc4058e8b8fff469af34d5e9
meso272 <liujinyang1201@126.com>
2023-07-14 19:51:34 -0700

    2dslices

--- include/QoZ/compressor/SZInterpolationCompressor.hpp
+++ include/QoZ/compressor/SZInterpolationCompressor.hpp
@@ -9132,10 +9132,27 @@ namespace QoZ {
                     
                     
                     std::array<size_t, N> begin_idx=begin,end_idx=end;
+                    /*
                     predict_error += block_interpolation_3d_crossblock(data, begin_idx,
                                                                 end_idx,std::array<size_t,3>{dims[0],dims[1],dims[2]},
-                                                                stride , interp_func, pb,dim_coeffs,meta,cross_block,tuning);
-                                                                
+                                                                stride , interp_func, pb,dim_coeffs,meta,cross_block,tuning);*/
+                    size_t begin_offset1 = begin[dims[0]] * dimension_offsets[dims[0]] ;
+                    size_t begin_offset2 = begin[dims[1]] * dimension_offsets[dims[1]] ;
+                    size_t begin_offset3 =  begin[dims[2]] * dimension_offsets[dims[2]];
+                    predict_error += block_interpolation_3d(data, begin_offset1,
+                                                                begin_offset1 +
+                                                                (end[dims[0]] - begin[dims[0]]) *
+                                                                dimension_offsets[dims[0]],
+                                                                begin_offset2,
+                                                                begin_offset2 +
+                                                                (end[dims[1]] - begin[dims[1]]) *
+                                                                dimension_offsets[dims[1]],
+                                                                begin_offset3,
+                                                                begin_offset3 +
+                                                                (end[dims[2]] - begin[dims[2]]) *
+                                                                dimension_offsets[dims[2]],
+                                                                stride * dimension_offsets[dims[0]],stride * dimension_offsets[dims[1]], stride * dimension_offsets[dims[2]],interp_func,pb,dim_coeffs,meta,tuning);//dim_coeffs
+                                                            
 
             }
             return predict_error;
